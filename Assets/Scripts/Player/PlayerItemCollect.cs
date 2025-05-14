64df8be2251d0974d3b7ae37e42269a06815c538
GodlyHamster <wouter.van.dompselaar@gmail.com>
2025-05-14 15:44:04 +0200

    itempickup now gets attracted to player

--- Assets/Scripts/Player/PlayerItemCollect.cs
+++ Assets/Scripts/Player/PlayerItemCollect.cs
@@ -1,12 +1,43 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 public class PlayerItemCollect : MonoBehaviour
 {
+    [SerializeField]
+    private float pickupRange;
+
+    private Dictionary<ItemPickup, Transform> itemPickupsInRange = new Dictionary<ItemPickup, Transform>();
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent<ItemPickup>(out ItemPickup item))
         {
-            item.Pickup();
+            itemPickupsInRange.Add(item, collision.transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<ItemPickup>(out ItemPickup item))
+        {
+            itemPickupsInRange.Remove(item);
+        }
+    }
+
+    private void Update()
+    {
+        var itemsTopickup = itemPickupsInRange.Where(x => Vector3.Distance(transform.position, x.Value.position) <= pickupRange).ToArray();
+        foreach (var item in itemsTopickup)
+        {
+            item.Key.Pickup();
+            itemPickupsInRange.Remove(item.Key);
+        }
+        foreach (var item in itemPickupsInRange)
+        {
+            Vector3 itemPos = item.Value.position;
+            Vector3 moveDirection = (transform.position - itemPos).normalized;
+            item.Value.position += moveDirection * 2 * Time.deltaTime;
         }
     }
 }
